ad9c56266d9bc6abc3b54faf1b7070a70d7e65de
Dafa Bintang Athaillah <55053091+Dafaathaillah@users.noreply.github.com>
2022-08-18 12:08:15 +0800

    new update admin

--- resources/views/layouts/layout_admin/sidebar.blade.php
+++ resources/views/layouts/layout_admin/sidebar.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <!--
 Template Name: NobleUI - Admin & Dashboard Template
@@ -76,70 +75,3 @@ License: You must have a valid license purchased only from above link or https:/
 	</nav> --}}
 
 </body>
-=======
-@section('footer')
-<nav class="sidebar">
-	<div class="sidebar-header">
-		<a href="#" class="sidebar-brand">
-			SAW<span>web</span>
-		</a>
-		<div class="sidebar-toggler not-active">
-			<span></span>
-			<span></span>
-			<span></span>
-		</div>
-	</div>
-	<div class="sidebar-body">
-		<ul class="nav">
-			<li class="nav-item nav-category">SIDEBAR - MENU</li>
-			<li class="nav-item">
-				<a href="#" class="nav-link">
-					<i class="link-icon" data-feather="box"></i>
-					<span class="link-title">Dashboard</span>
-				</a>
-			</li>
-			<li class="nav-item">
-				<a href="#" class="nav-link">
-					<i class="link-icon" data-feather="code"></i>
-					<span class="link-title">Manage User</span>
-				</a>
-			</li>
-			<li class="nav-item">
-				<a href="#" class="nav-link">
-					<i class="link-icon" data-feather="book"></i>
-					<span class="link-title">Manage Agency</span>
-				</a>
-			</li>
-			<li class="nav-item">
-				<a href="#" class="nav-link">
-					<i class="link-icon" data-feather="minus-square"></i>
-					<span class="link-title">Manage Ads</span>
-				</a>
-			</li>
-			<li class="nav-item">
-				<a href="#" class="nav-link">
-					<i class="link-icon" data-feather="edit-3"></i>
-					<span class="link-title">Manage T&C</span>
-				</a>
-			</li>
-			<li class="nav-item">
-				<a href="#" class="nav-link">
-					<i class="link-icon" data-feather="pie-chart"></i>
-					<span class="link-title">Manage Logo</span>
-				</a>
-			</li>
-		</ul>
-	</div>
-</nav>
-{{-- <nav class="settings-sidebar">
-	<div class="sidebar-body">
-		<a href="#" class="settings-sidebar-toggler">
-			<i data-feather="settings"></i>
-		</a>
-		<div class="theme-wrapper">
-			<h6 class="text-muted mb-2">Mohon Maaf Fitur Masih Dalam Tahap Pengembangan!</h6>
-		</div>
-	</div>
-</nav> --}}
-@show
->>>>>>> main
