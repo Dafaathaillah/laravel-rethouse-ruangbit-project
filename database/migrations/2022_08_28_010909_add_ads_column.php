0e719c7fca527117d376ebf38570f59d6b196794
ahmadhafis <ahmadhafis296@gmail.com>
2022-08-28 08:35:19 +0700

    ads migration

---
+++ database/migrations/2022_08_28_010909_add_ads_column.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddAdsColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('property', function (Blueprint $table) {
+            $table->integer('ads_list_id')->unsigned()->nullable();
+
+            $table->foreign('ads_id')->references('id')->on('ad_lists')->onDelete('NULL');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('property', function (Blueprint $table) {
+            $table->dropForeign(['ads_id']);
+
+            // 2. Drop the column
+            $table->dropColumn('ads_id');
+        });
+    }
+}
