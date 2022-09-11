9896f0e5f3995a237b4e065c7b2b2d83d3b27565
ahmadhafis <ahmadhafis296@gmail.com>
2022-09-11 21:08:21 +0700

    Create property view

--- resources/views/layouts/layout/master_main_create.blade.php
+++ resources/views/layouts/layout/master_main_create.blade.php
@@ -27,6 +27,15 @@
     <link rel="apple-touch-icon" href="{{ asset('icon.png') }}">
     <meta name="theme-color" content="#3454d1">
     <link href="{{ asset('./css/styles.css?fd365619e86ad9137a29') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.css"
+        integrity="sha512-zoIoZAaHj0iHEOwZZeQnGqpU8Ph4ki9ptyHZFPe+BmILwqAksvwm27hR9dYH4WXjYY/4/mz8YDBCgVqzc2+BJA=="
+        crossorigin="anonymous"/>
+    <style>
+        .dz-image img {
+            width: 120px;
+            height: 120px;
+        }
+    </style>
 </head>
 
 <body>
@@ -44,6 +53,9 @@
     <a href="{{ asset('javascript:') }}" id="return-to-top"><i class="fa fa-chevron-up"></i></a>
     <!-- END SCROLL TO TOP -->
     <script src="{{ asset('./js/index.bundle.js?fd365619e86ad9137a29') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.js"
+        integrity="sha512-jytq61HY3/eCNwWirBhRofDxujTCMFEiQeTe+kHR4eYLNTXrUq7kY2qQDKOUnsVAKN5XGBJjQ3TvNkIkW/itGw=="
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
--- resources/views/user/property/create_property.blade.php
+++ resources/views/user/property/create_property.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.layout.master_main_create')
-@include('sweetalert::alert')
+
 @section('title')
     | Create Property
 @endsection
@@ -13,31 +13,39 @@
                         Add Property
                     </div>
                     <div class="card-body">
-                        @if ($errors->any())
-                        <div class="alert alert-danger">
-                            <strong>Whoops!</strong> There were some problems with your input.<br><br>
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                            <strong>Please fill all field above </strong>
-                        </div>
-                        @endif
-                        <form method="post" action="{{ route('property.store') }}" id="myForm" enctype="multipart/form-data">
+                        <form method="post" action="{{ route('property.store') }}" id="myForm"
+                            enctype="multipart/form-data">
                             @csrf
                             <div style="margin-top: 20px" class="form-group col-12">
                                 <label for="picture">Picture</label>
-                                <div class="input-group">
-                                    <input type="file" name="image" class="form-control @error('image') is-invalid @enderror" id="image"
-                                        aria-describedby="image">
-                                    <label for="image" class="input-group-text" style="line-height: 1.1rem">Upload</label>
+                                <div class="dropzone dz-clickable @error('image') is-invalid @enderror" name="image[]"
+                                    id="image">
+                                    <div class="dz-default dz-message"><span>Drop images here</span></div>
                                     @error('image')
                                         <div class="invalid-feedback">
                                             {{ $message }}
                                         </div>
                                     @enderror
                                 </div>
+                                <script type="text/javascript">
+                                    Dropzone.options.dropzone = {
+                                        maxFilesize: 12,
+                                        renameFile: function(file) {
+                                            var dt = new Date();
+                                            var time = dt.getTime();
+                                            return time + file.name;
+                                        },
+                                        acceptedFiles: ".jpeg,.jpg,.png,.gif",
+                                        addRemoveLinks: true,
+                                        timeout: 5000,
+                                        success: function(file, response) {
+                                            console.log(response);
+                                        },
+                                        error: function(file, response) {
+                                            return false;
+                                        }
+                                    };
+                                </script>
                             </div>
                             <div style="margin-top: 20px" class="form-group col-12 ">
                                 <label for="name">Name</label>
@@ -57,36 +65,39 @@
                                     <option value="Sold OUt">Sold out</option>
                                 </select>
                             </div>
-                            <div style="margin-top: 20px" class="form-group col-12 ">
+                            <div style="margin-top: 20px" class="form-group col-md-12 ">
                                 <label for="street">Address</label>
                                 <input type="text" class="form-control" id="street" name="street"
                                     placeholder="Address">
                             </div>
-                            <div style="margin-top: 20px" class="form-group col-12 ">
-                                <label for="city_id">City</label>
-                                <select class="form-control" id="city_id" name="city_id">
-                                    <option value="#" disabled selected>Choose City</option>
-                                    @foreach ($city as $cty)
-                                        {
-                                        <option value="<?= $cty->id ?>">
-                                            <?= $cty->name ?>
-                                        </option>
-                                        }
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div style="margin-top: 20px" class="form-group col-12 ">
-                                <label for="provinsi">Provinsi</label>
-                                <select class="form-control" id="provience_id" name="provience_id">
-                                    <option value="#" disabled selected>Choose Provience</option>
-                                    @foreach ($province as $prov)
-                                        {
-                                        <option value="<?= $prov->id ?>">
-                                            <?= $prov->name ?>
-                                        </option>
-                                        }
-                                    @endforeach
-                                </select>
+                            <div class="row">
+                                &nbsp;&nbsp;&nbsp;&nbsp;
+                                <div class="form-group col-md-5 ">
+                                    <label for="city_id">City</label>
+                                    <select class="form-control" id="city_id" name="city_id">
+                                        <option value="#" disabled selected>Choose City</option>
+                                        @foreach ($city as $cty)
+                                            {
+                                            <option value="<?= $cty->id ?>">
+                                                <?= $cty->name ?>
+                                            </option>
+                                            }
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <div class="form-group col-md-5 ">
+                                    <label for="provinsi">Province</label>
+                                    <select class="form-control" id="provience_id" name="provience_id">
+                                        <option value="#" disabled selected>Choose Provience</option>
+                                        @foreach ($province as $prov)
+                                            {
+                                            <option value="<?= $prov->id ?>">
+                                                <?= $prov->name ?>
+                                            </option>
+                                            }
+                                        @endforeach
+                                    </select>
+                                </div>
                             </div>
                             <div style="margin-top: 20px" class="form-group col-12 ">
                                 <label for="deskripsi">Description Property</label>
@@ -105,55 +116,18 @@
                                     @endforeach
                                 </select>
                             </div>
-                            <div style="margin-top: 20px" class="form-group col-md-12">
-                                <label for="picture">Add ADS</label>
-                                <div class="input-group">
-                                    <button class="btn btn-primary" type="button" data-toggle="collapse"
-                                        data-target="#adscard" aria-expanded="false" aria-controls="collapseExample">
-                                        Click this button if you want to show list ads
-                                    </button>
-                                    <div class="collapse" id="adscard">
-                                        <div class="row">
-                                            @foreach ($ad_lists as $ads)
-                                                <div class="col-md-6 mt-3">
-                                                    <div class="card bg-light" style="width: 20rem">
-                                                        <div class="card-body text-center">
-                                                            <h5 class="card-title">{{ $ads->title }}</h5>
-                                                            <p class="card-text">{{ $ads->description }}</p>
-                                                            <p class="card-text"><small class="text-muted">Rp {{ $ads->ad_price }}</small>
-                                                            </p>
-                                                        </div>
-                                                        <div class="card-footer">
-                                                            <div class="form-check form-check-inline">
-                                                                <input class="form-check-input" type="radio"
-                                                                    name="ads_id" id="ads_id"
-                                                                    value="<?= $ads->id ?>">
-                                                                <label class="form-check-label"
-                                                                    for="ads_id">Pilih</label>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            @endforeach
-
-                                        </div>
-                                        <div class="card">
-                                            <div class="card-body">
-                                                <label for="image_transaction">Receipt of Payment</label>
-                                                <div class="input-group">
-                                                    <input type="file" name="image_transaction" class="form-control @error('image_transaction') is-invalid @enderror" id="image_transaction"
-                                                        aria-describedby="image_transaction">
-                                                    <label for="image_transaction" class="input-group-text" style="line-height: 1.1rem">Upload</label>
-                                                    @error('image_transaction')
-                                                        <div class="invalid-feedback">
-                                                            {{ $message }}
-                                                        </div>
-                                                    @enderror
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                            <div style="margin-top: 20px" class="form-group col-12 ">
+                                <label for="type_property_id">Advertise</label>
+                                <select class="form-control" id="type_property_id" name="type_property_id">
+                                    <option value="#" disabled selected>Choose Advertise</option>
+                                    @foreach ($ad_lists as $ads)
+                                        {
+                                        <option value="<?= $ads->id ?>">
+                                            <?= $ads->title ?>
+                                        </option>
+                                        }
+                                    @endforeach
+                                </select>
                             </div>
                             <div class="card">
                                 <div class="card-body">
@@ -161,54 +135,34 @@
                                         <div class="form-group col-md-4">
                                             <label for="bedroom">Bedroom</label>
                                             <input type="number" class="form-control" id="bedroom" name="bedroom">
-                                          </div>
-                                          <div class="form-group col-md-4">
+                                        </div>
+                                        <div class="form-group col-md-4">
                                             <label for="bathroom">Bathroom</label>
                                             <input type="number" class="form-control" id="bathroom" name="bathroom">
-                                          </div>
-                                          <div class="form-group col-md-4">
+                                        </div>
+                                        <div class="form-group col-md-4">
                                             <label for="garage">Garage</label>
                                             <input type="number" class="form-control" id="garage" name="garage">
-                                          </div>
-                                          <div class="form-group col-md-4">
+                                        </div>
+                                        <div class="form-group col-md-4">
                                             <label for="property_size">Property Size</label>
-                                            <input type="text" class="form-control" id="property_size" name="property_size">
-                                          </div>
-                                          <div class="form-group col-md-4">
+                                            <input type="text" class="form-control" id="property_size"
+                                                name="property_size">
+                                        </div>
+                                        <div class="form-group col-md-4">
                                             <label for="area">Area</label>
                                             <input type="text" class="form-control" id="area" name="area">
-                                          </div>
+                                        </div>
+                                        <div style="margin-top: 20px" class="form-group col-12 ">
+                                            <label for="features">Features</label>
+                                            <textarea class="form-control" id="features" name="features" placeholder="Features of Property"></textarea>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="card">
-                                <div class="card-body">
-                                    <div style="margin-top: 20px" class="form-group col-12 ">
-                                        <label for="features">Features</label>
-                                        <textarea class="form-control" id="features" name="features" placeholder="Features of Property"></textarea>
-                                    </div>
-                                </div>
-                            </div>
-                            {{-- <div style="margin-top: 20px" class="form-group col-12 col-sm-4">
-                            <label for="formFile" class="form-label">Photo Property</label>
-                            <input class="form-control" @error('image') is-invalid @enderror type="file" id="image"
-                                name="image">
-                            @error('image')
-                            <div class="invalid-feedback">
-                                {{ $message }}
-                            </div>
-                            @enderror
-                        </div> --}}
-                            <div class="custom-control custom-switch">
-                                <input type="checkbox" class="custom-control-input" id="customSwitch1">
-                                <label class="custom-control-label" for="customSwitch1">Advertise this Property</label>
                             </div>
                             <div class="row justify-content-end">
                                 <button type="submit" style="margin-top: 20px" class="btn btn-primary">Simpan</button>
                                 &nbsp;
-                                {{-- <a style="margin-top: 20px" type="submit" class="btn btn-success btn-block">
-                                Simpan
-                            </a> --}}
                                 <a href="{{ route('property.index') }}" style="margin-top: 20px" type="button"
                                     class="btn btn-warning">
                                     Cancel
