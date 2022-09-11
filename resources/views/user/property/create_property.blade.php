66e45e4b8b057125bcb85431e270a18eb346be7a
Dafa Bintang Athaillah <55053091+Dafaathaillah@users.noreply.github.com>
2022-09-12 01:08:02 +0800

    update form create property (add listing)

--- resources/views/user/property/create_property.blade.php
+++ resources/views/user/property/create_property.blade.php
@@ -8,7 +8,7 @@
     <div class="container mt-5">
         <div class="row justify-content-center align-items-center">
             <div class="mb-3">
-                <div class="card mb-3 rounded" style="width: 48rem;">
+                <div class="shadow card mb-3 rounded" style="width: 48rem;">
                     <div class="card-header bg-primary text-white">
                         Add Property
                     </div>
@@ -16,9 +16,9 @@
                         <form method="post" action="{{ route('property.store') }}" id="myForm"
                             enctype="multipart/form-data">
                             @csrf
-                            <div style="margin-top: 20px" class="form-group col-12">
+                            <div style="mt-3 mb-3" class="form-group col-12">
                                 <label for="picture">Picture</label>
-                                <div class="dropzone dz-clickable @error('image') is-invalid @enderror" name="image[]"
+                                <div class="shadow dropzone dz-clickable @error('image') is-invalid @enderror" name="image[]"
                                     id="image">
                                     <div class="dz-default dz-message"><span>Drop images here</span></div>
                                     @error('image')
@@ -47,124 +47,119 @@
                                     };
                                 </script>
                             </div>
-                            <div style="margin-top: 20px" class="form-group col-12 ">
-                                <label for="name">Name</label>
-                                <input type="text" class="form-control" id="name" name="name" placeholder="Name">
-                            </div>
-                            <div style="margin-top: 20px" class="form-group col-12 ">
-                                <label for="price">Price</label>
-                                <input type="price" class="form-control" id="price" name="price"
-                                    placeholder="Price">
-                            </div>
-                            <div style="margin-top: 20px" class="form-group col-12 ">
-                                <label for="status_property" class="form-label">Status</label>
-                                <select class="form-control" id="status_property" name="status_property">
-                                    <option value="#" disabled selected>Choose Status</option>
-                                    <option value="For Sell">For Sell</option>
-                                    <option value="For Rent">For Rent</option>
-                                    <option value="Sold OUt">Sold out</option>
-                                </select>
-                            </div>
-                            <div style="margin-top: 20px" class="form-group col-md-12 ">
-                                <label for="street">Address</label>
-                                <input type="text" class="form-control" id="street" name="street"
-                                    placeholder="Address">
-                            </div>
-                            <div class="row">
-                                &nbsp;&nbsp;&nbsp;&nbsp;
-                                <div class="form-group col-md-5 ">
-                                    <label for="city_id">City</label>
-                                    <select class="form-control" id="city_id" name="city_id">
-                                        <option value="#" disabled selected>Choose City</option>
-                                        @foreach ($city as $cty)
-                                            {
-                                            <option value="<?= $cty->id ?>">
-                                                <?= $cty->name ?>
-                                            </option>
-                                            }
-                                        @endforeach
-                                    </select>
-                                </div>
-                                <div class="form-group col-md-5 ">
-                                    <label for="provinsi">Province</label>
-                                    <select class="form-control" id="provience_id" name="provience_id">
-                                        <option value="#" disabled selected>Choose Provience</option>
-                                        @foreach ($province as $prov)
-                                            {
-                                            <option value="<?= $prov->id ?>">
-                                                <?= $prov->name ?>
-                                            </option>
-                                            }
-                                        @endforeach
-                                    </select>
-                                </div>
-                            </div>
-                            <div style="margin-top: 20px" class="form-group col-12 ">
-                                <label for="deskripsi">Description Property</label>
-                                <textarea class="form-control" id="description" name="description" placeholder="Description Your Property"></textarea>
-                            </div>
-                            <div style="margin-top: 20px" class="form-group col-12 ">
-                                <label for="type_property_id">Type Property</label>
-                                <select class="form-control" id="type_property_id" name="type_property_id">
-                                    <option value="#" disabled selected>Choose Type Property</option>
-                                    @foreach ($type_property as $type)
-                                        {
-                                        <option value="<?= $type->id ?>">
-                                            <?= $type->name ?>
-                                        </option>
-                                        }
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div style="margin-top: 20px" class="form-group col-12 ">
-                                <label for="type_property_id">Advertise</label>
-                                <select class="form-control" id="type_property_id" name="type_property_id">
-                                    <option value="#" disabled selected>Choose Advertise</option>
-                                    @foreach ($ad_lists as $ads)
-                                        {
-                                        <option value="<?= $ads->id ?>">
-                                            <?= $ads->title ?>
-                                        </option>
-                                        }
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div class="card">
-                                <div class="card-body">
-                                    <div class="row">
-                                        <div class="form-group col-md-4">
-                                            <label for="bedroom">Bedroom</label>
-                                            <input type="number" class="form-control" id="bedroom" name="bedroom">
-                                        </div>
-                                        <div class="form-group col-md-4">
-                                            <label for="bathroom">Bathroom</label>
-                                            <input type="number" class="form-control" id="bathroom" name="bathroom">
-                                        </div>
-                                        <div class="form-group col-md-4">
-                                            <label for="garage">Garage</label>
-                                            <input type="number" class="form-control" id="garage" name="garage">
-                                        </div>
-                                        <div class="form-group col-md-4">
-                                            <label for="property_size">Property Size</label>
-                                            <input type="text" class="form-control" id="property_size"
-                                                name="property_size">
-                                        </div>
-                                        <div class="form-group col-md-4">
-                                            <label for="area">Area</label>
-                                            <input type="text" class="form-control" id="area" name="area">
-                                        </div>
-                                        <div style="margin-top: 20px" class="form-group col-12 ">
-                                            <label for="features">Features</label>
-                                            <textarea class="form-control" id="features" name="features" placeholder="Features of Property"></textarea>
-                                        </div>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="form-group col-4 ">
+                                        <label for="name">Name</label>
+                                        <input type="text" class="form-control shadow-sm" id="name" name="name" placeholder="Name">
+                                    </div>
+                                    <div class="form-group col-4 ">
+                                        <label for="price">Price</label>
+                                        <input type="price" class="form-control shadow-sm" id="price" name="price"
+                                            placeholder="Price">
+                                    </div>
+                                    <div class="form-group col-4 ">
+                                        <label for="status_property" class="form-label shadow-sm">Status</label>
+                                        <select class="form-control" id="status_property" name="status_property">
+                                            <option value="#" disabled selected>Choose Status</option>
+                                            <option value="For Sell">For Sell</option>
+                                            <option value="For Rent">For Rent</option>
+                                            <option value="Sold OUt">Sold out</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-md-4 ">
+                                        <label for="street">Address</label>
+                                        <input type="text" class="form-control shadow-sm" id="street" name="street"
+                                            placeholder="Address">
+                                    </div>
+                                    <div class="form-group col-4 ">
+                                        <label for="city_id">City</label>
+                                        <select class="form-control shadow-sm" id="city_id" name="city_id">
+                                            <option value="#" disabled selected>Choose City</option>
+                                            @foreach ($city as $cty)
+                                                {
+                                                <option value="<?= $cty->id ?>">
+                                                    <?= $cty->name ?>
+                                                </option>
+                                                }
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-4 ">
+                                        <label for="provinsi">Province</label>
+                                        <select class="form-control shadow-sm" id="provience_id" name="provience_id">
+                                            <option value="#" disabled selected>Choose Provience</option>
+                                            @foreach ($province as $prov)
+                                                {
+                                                <option value="<?= $prov->id ?>">
+                                                    <?= $prov->name ?>
+                                                </option>
+                                                }
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-12 ">
+                                        <label for="deskripsi">Description Property</label>
+                                        <textarea class="form-control shadow-sm" id="description" name="description" placeholder="Description Your Property"></textarea>
+                                    </div>
+                                    <div class="form-group col-6 ">
+                                        <label for="type_property_id">Type Property</label>
+                                        <select class="form-control shadow-sm" id="type_property_id" name="type_property_id">
+                                            <option value="#" disabled selected>Choose Type Property</option>
+                                            @foreach ($type_property as $type)
+                                                {
+                                                <option value="<?= $type->id ?>">
+                                                    <?= $type->name ?>
+                                                </option>
+                                                }
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-6 ">
+                                        <label for="type_property_id">Advertise</label>
+                                        <select class="form-control shadow-sm" id="type_property_id" name="type_property_id">
+                                            <option value="#" disabled selected>Choose Advertise</option>
+                                            @foreach ($ad_lists as $ads)
+                                                {
+                                                <option value="<?= $ads->id ?>">
+                                                    <?= $ads->title ?>
+                                                </option>
+                                                }
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-6">
+                                        <label for="bedroom">Bedroom</label>
+                                        <input type="number" class="form-control shadow-sm" id="bedroom" name="bedroom">
+                                    </div>
+                                    <div class="form-group col-6">
+                                        <label for="bathroom">Bathroom</label>
+                                        <input type="number" class="form-control shadow-sm" id="bathroom" name="bathroom">
+                                    </div>
+                                    <div class="form-group col-6">
+                                        <label for="garage">Garage</label>
+                                        <input type="number" class="form-control shadow-sm" id="garage" name="garage">
+                                    </div>
+                                    <div class="form-group col-6">
+                                        <label for="property_size">Property Size</label>
+                                        <input type="text" class="form-control shadow-sm" id="property_size"
+                                            name="property_size">
+                                    </div>
+                                    <div class="form-group col-12">
+                                        <label for="area">Area</label>
+                                        <input type="text" class="form-control shadow-sm" id="area" name="area">
+                                    </div>
+                                    <div style="margin-top: 20px" class="form-group col-12 ">
+                                        <label for="features">Features</label>
+                                        <textarea class="form-control shadow-sm" id="features" name="features" placeholder="Features of Property"></textarea>
                                     </div>
                                 </div>
                             </div>
                             <div class="row justify-content-end">
-                                <button type="submit" style="margin-top: 20px" class="btn btn-primary">Simpan</button>
+                                <button type="submit" style="margin-top: 20px" class="btn btn-primary shadow rounded">Simpan</button>
                                 &nbsp;
                                 <a href="{{ route('property.index') }}" style="margin-top: 20px" type="button"
-                                    class="btn btn-warning">
+                                    class="btn btn-warning shadow rounded">
                                     Cancel
                                 </a>
                             </div>
