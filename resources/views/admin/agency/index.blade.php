f93b34df00031f6899aca9249a1f34a7dae75169
Dafa Bintang Athaillah <55053091+Dafaathaillah@users.noreply.github.com>
2022-09-12 10:34:24 +0800

    update agency

--- resources/views/admin/agency/index.blade.php
+++ resources/views/admin/agency/index.blade.php
@@ -22,7 +22,7 @@
                 {{-- <p class="card-description">Read the <a href="https://datatables.net/" target="_blank"> Official
                         DataTables Documentation </a>for a full list of instructions and other options.</p> --}}
                 <br>
-                <a class="btn btn-primary" href="{{ route('admin.agency.create') }}"><i class="link-icon"
+                <a class="btn btn-primary px-1 py-1" href="{{ route('admin.agency.create') }}"><i class="link-icon p-1"
                         data-feather="plus"></i>Add Agency</a>
                 <div class="table-responsive">
                     <table id="dataTableExample" class="table">
@@ -39,13 +39,15 @@
                                 <td>{{ ++$no; }}</td>
                                 <td>{{ $agn->name }}</td>
                                 <td>
-                                    <a class="btn btn-success" href="{{ route('admin.agency.show',$agn->id) }}"><i
-                                            class="link-icon" data-feather="eye"></i></a>
-                                    <a class="btn btn-primary" href="{{ route('admin.agency.edit',$agn->id) }}"><i
-                                            class="link-icon" data-feather="edit"></i></a>
-                                    <a class="btn btn-primary delete" href="#" id="trash" data-id="{{ $agn->id }}"
-                                        data-name="{{ $agn->name }}"><i class="link-icon"
-                                            data-feather="trash-2"></i></a>
+                                    <a class="btn btn-primary px-1 py-1"
+                                        href="{{ route('admin.agency.show',$agn->id) }}"><i class="link-icon p-1"
+                                            data-feather="eye"></i></a>
+                                    <a class="btn btn-primary px-1 py-1"
+                                        href="{{ route('admin.agency.edit',$agn->id) }}"><i class="link-icon p-1"
+                                            data-feather="edit"></i></a>
+                                    <a class="btn btn-primary px-1 py-1 delete" href="#" id="trash"
+                                        data-id="{{ $agn->id }}" data-name="{{ $agn->name }}"><i class="link-icon p-1"
+                                            data-feather="trash"></i></a>
                                 </td>
                                 {{-- <td>
                                     <a href="#" class="nav-link">
